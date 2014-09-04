64a064b6fd3054cc1a3e5b7fb7fbf10f02c443a2
Shelby Kelly <shelby.kelly@colorado.edu>
2014-09-04 15:10:34 -0600

    code reformat

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ActiveRecord::Base
 
   belongs_to :task_list
 
-  validates :task, presence:{:message => "Your task could not be created"}
+  validates :task, presence: {:message => "Your task could not be created"}
 
 end
